90f8d7124453c2b06d6a51bac44826bb382cf018
Yertas <yertas.zhumabekov@gmail.com>
2026-09-17 17:00:01 +0500

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_table = {}
+        for i  in range(len(nums)):
+            difference = 0
+            difference = target - nums[i]
+            if difference not in hash_table:
+                hash_table[nums[i]] = i
+            else:
+                return [hash_table[difference],i]
+            
